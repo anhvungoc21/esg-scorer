84e1415080927266f839413d311257d09fcc2fd6
Anh Vu <anhvungoc.21@gmail.com>
2022-09-24 16:21:00 -0500

    head still failing?

--- pages/about-esg.js
+++ pages/about-esg.js
@@ -1,7 +1,7 @@
-import Head from "next/head";
 import NavBar from "../components/NavBar";
+import Head from "next/head";
 
-export default function AboutESG() {
+export default function HowToUse() {
   return (
     <div className="h-screen w-screen grid grid-rows-6 bg-white">
       <Head>
@@ -9,7 +9,7 @@ export default function AboutESG() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <NavBar />
-      About ESG
+      AboutESG
     </div>
   );
 }
