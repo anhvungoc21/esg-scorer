cd5a1b6c726b900edd07afbdad37cfab3fa42964
Anh Vu <anhvungoc.21@gmail.com>
2022-09-25 11:59:25 -0500

    restructured

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/documentation":["static\u002Fchunks\u002Fpages\u002Fdocumentation.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdocumentation"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
